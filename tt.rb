b07b80fab532c07590529f1f756a813505834729
William Diebolt <wmdiebolt@gmail.com>
2023-10-23 20:39:18 -0700

    improved features and fixed substring bug

--- tt.rb
+++ tt.rb
@@ -1,39 +1,44 @@
 require 'io/console'
-
-def rand_char
-  (("a".ord) + rand(26)).chr
-end
-
-def rand_word n
-  word = ''
-  for _ in 1..n do word += rand_char end
-  return word
-end
-
-def user_attempt target
-  puts target
-  input = ''
-  while (input != target) && target[input] do
-    system("stty raw -echo")
-    input += STDIN.getc
-    system("stty -raw echo")
-    if input[-1] == '~' then exit end
-  end
-  return input
-end
-
-def main
-  puts "Exit with tilde: ~"
-  word_length = 1
-  while true do
-    target = rand_word word_length
-    if (user_attempt target) != target
-      while (user_attempt target) != target do end
-      word_length = 0
-    else
-      word_length += 1 
+ 
+class TouchTyper
+  def initialize
+    word_length = 1
+    loop do
+      target = rand_word word_length
+      if (user_attempt target) != target 
+        while (user_attempt target) != target do end
+        word_length = 0
+      else
+        word_length += 1 
+      end
     end
+  end   
+ 
+  def rand_char
+    (("a".ord) + rand(26)).chr
+  end
+  
+  def rand_word n
+    word = ''
+    n.times { word += rand_char }
+    return word
+  end
+  
+  def user_attempt target
+    puts target
+    input = ''
+    i = 0
+    while (input != target) && (target[0...i] == input[0...i]) do
+      system("stty raw echo")
+      input += STDIN.getc
+      system("stty -raw echo")
+      if input[-1].ord == 3 then exit end #checks for ^C 
+      i += 1
+    end
+    puts
+    return input
   end
 end
 
-main #entry point
+TouchTyper.new  
+ 
